c281ce5dbe03979fe4ea3bbe61963029734ada3e
degi100 <r.degering@mail.de>
2021-12-19 22:13:35 +0100

    add smth

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ recognition.onresult = function (event) {
 function computerSpeech(words) {
   const speech = new SpeechSynthesisUtterance();
   speech.lang = "de";
-//   speech.lang = "en-US";
+  //   speech.lang = "en-US";
   speech.pitch = 0.9;
   speech.volume = 1;
   speech.rate = 1;
@@ -31,9 +31,15 @@ function computerSpeech(words) {
 }
 
 function determineWords(speech, words) {
-  if (words.includes("wie geht es dir" || "how are you")) {
+  if (words.includes("wie geht es dir")) {
     speech.text = "Mir geht es gut, danke! Und Dir?";
   }
+
+  if (words.includes("Hallo")) {
+    speech.text = "hallo... , ich bin ein programmierter Bot,.... Bisher kann ich nur wenige Kommandos. Aber mein Entwickler, bringt mir jeden Tag neue Dinge bei. Wähle unten aus der Liste etwas aus, und ich werde mit dir interagieren."
+   
+  }
+
   if (words.includes("wie ist das Wetter")) {
     speech.text = "bleibe lieber zu Hause, denn dort ist es am schönsten";
   }
@@ -43,9 +49,13 @@ function determineWords(speech, words) {
   if (words.includes("was machst du heute")) {
     speech.text = "ich lerne künstliche intelligenz, ha ha ha";
   }
+  if (words.includes("öffne youtube")) {
+    speech.text = "youtube wird geöffnet";
+    window.open("http://www.youtube.de");
+  }
   if (words.includes("öffne google")) {
     speech.text = "google wird geöffnet";
-	window.open('http://www.google.de')
+    window.open("http://www.google.de");
   }
 }
 
